6912293f14cef1643d5b65cfe1b580f2c0bc3286
Supamoto-it <44291482+DumisaniLesley@users.noreply.github.com>
2024-01-16 09:30:54 +0200

    added icon to the outline button

--- components/ButtonOutline.jsx
+++ components/ButtonOutline.jsx
@@ -1,12 +1,16 @@
 import Link from 'next/link'
 import React from 'react'
 
+
 // Adjust Font Size
 
-const ButtonOutline = ({ url, title}) => {
+const ButtonOutline = ({ icon, url, title}) => {
   return (
     <button className='border-2 border-[#523728] py-1 px-4 lg:py-2 lg:px-6 rounded-3xl hover:bg-[#523728] hover:text-[#FFF8E1] transition-all duration-200 ease-out font-semibold tracking-wide'>
-      <Link href={url}>{title}</Link>
+      <div className='flex items-center gap-2'>
+        {icon}
+        <Link href={url}>{title}</Link>
+      </div>
     </button>
   )
 }
--- components/Hero.jsx
+++ components/Hero.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Image from 'next/image'
 import ButtonOutline from './ButtonOutline'
 import Button from './Button'
+import { FaPlay } from 'react-icons/fa'
 
 const Hero = () => {
   return (
@@ -11,7 +12,7 @@ const Hero = () => {
         <p className='text-lg mt-6 max-w-[500px]'>We always make our customer happy by providing as many choices as possible.</p>
         <div className='mt-6 mx-auto lg:mx-0'>
           <Button url={'/'} title='Get Started'/>
-          <ButtonOutline url='/' title={'Watch Demo'}/>
+          <ButtonOutline icon={<FaPlay/>} url='/' title={'Watch Demo'}/>
         </div>
       </div>
       <div className='max-w-[500px] lg:max-w-[650px] '>
